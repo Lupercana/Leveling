31bf55845c88dca0703570c3feb11c34d4ad42c9
Bo <zboyang@umich.edu>
2020-05-18 18:20:52 -0400

    Started Saving scripts

---
+++ Assets/Scripts/Data_Save/Save_Serial_Data.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class Save_Serial_Data
+{
+    const string path_ending = "/Leveling/Save.dat";
+
+    public static void Save_Data()
+    {
+        BinaryFormatter bform = new BinaryFormatter();
+
+        string save_path = Application.persistentDataPath + path_ending;
+        FileStream stream = new FileStream(save_path, FileMode.Create); // Creates new file or overwrites existing
+
+        Serial_Data sdata = new Serial_Data();
+
+        bform.Serialize(stream, sdata);
+        stream.Close();
+    }
+
+    public static Serial_Data Load_Data()
+    {
+        string save_path = Application.persistentDataPath + path_ending;
+
+        if (File.Exists(save_path))
+        {
+            BinaryFormatter bform = new BinaryFormatter();
+            FileStream stream = new FileStream(save_path, FileMode.Open);
+
+            Serial_Data sdata = bform.Deserialize(stream) as Serial_Data;
+            stream.Close();
+
+            return sdata;
+        }
+        else
+        {
+            return new Serial_Data();
+        }
+    }
+}
---
+++ Assets/Scripts/Data_Save/Serial_Data.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Serial_Data
+{
+    public enum Item_Type
+    {
+        Melee,
+        Range, 
+        Magic,
+        Accessory,
+        Movement
+    }
+
+    public struct Item
+    {
+        Item_Type type;
+        string name;
+        int val1;
+        int val2;
+        int val3;
+    }
+
+    public struct Slot
+    {
+        Item slot_weapon;
+        Item slot_accessory1;
+        Item slot_accessory2;
+        Item slot_movement;
+    }
+
+    // Stats
+    public int stat_melee;
+    public int stat_range;
+    public int stat_magic;
+    public int stat_agility;
+
+    // Stat XP
+    public int xp_melee;
+    public int xp_range;
+    public int xp_magic;
+    public int xp_agility;
+
+    // Bank items
+    public List<Item> bank;
+
+    // Equip Slots
+    public Slot[] equip_slots;
+}
