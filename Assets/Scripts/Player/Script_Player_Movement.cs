fe4e3db5354720e3d2f5357ae7ff88bcb0192203
Bo <zboyang@umich.edu>
2020-05-20 18:49:45 -0400

    Player movement and BG setup

---
+++ Assets/Scripts/Player/Script_Player_Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Player_Movement : MonoBehaviour
+{
+    public Rigidbody2D ref_rbody;
+
+    void Update()
+    {
+        float h_axis = Input.GetAxis("Horizontal");
+        float v_axis = Input.GetAxis("Vertical");
+
+        ref_rbody.AddForce(new Vector2(h_axis, v_axis));
+    }
+}
