ddc34c3d749ccc127cb78c10fe6e1cc1d5c97eb9
Bo <zboyang@umich.edu>
2020-05-24 21:36:24 -0400

    Added scene transitions through town portal

---
+++ Assets/Scripts/Scene_Management/Script_Change_Scene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script_Change_Scene : MonoBehaviour
+{
+    [SerializeField]
+    private Object ref_next_scene = null;
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(ref_next_scene.name);
+    }
+}
---
+++ Assets/Scripts/Scene_Management/Script_Dont_Destroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Dont_Destroy : MonoBehaviour
+{
+    [SerializeField]
+    private string tag = null;
+
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag(tag);
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
